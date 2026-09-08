8dbb8a5fc928671a2ca625cb873c79722dd7cdb0
michaelddmanuel <michaelddmanuel@users.noreply.github.com>
2026-09-08 00:14:34 -0500

    Declutter: top is one line (title + mic + settings); remove quick-add bar/Add/+Column/common-items row; kill white space; unlock landscape (removed orientation lock); Add-column moved into Settings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE="mylist-v4";
+var CACHE="mylist-v5";
 var ASSETS=["./","index.html","manifest.webmanifest","icon.svg","icon-192.png","icon-512.png","sync.js"];
 self.addEventListener("install",function(e){ e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);})); self.skipWaiting(); });
 self.addEventListener("activate",function(e){ e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){return k!==CACHE?caches.delete(k):null;}));})); self.clients.claim(); });
