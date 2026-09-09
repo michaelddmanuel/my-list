c0b04b809e5f74078f6f61ea78fc04b59ef52806
michaelddmanuel <michaelddmanuel@users.noreply.github.com>
2026-09-09 02:23:38 -0500

    Stage 3 (find/organise): search + view filters (All/Open/Overdue/Done) + assignee filter + hide-ticked, persisted; filtered count; sw v10; +7 tests (21/21 pass)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE="mylist-v9";
+var CACHE="mylist-v10";
 var ASSETS=["./","index.html","manifest.webmanifest","icon.svg","icon-192.png","icon-512.png","sf-logo.png","sync.js"];
 self.addEventListener("install",function(e){ e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);})); self.skipWaiting(); });
 self.addEventListener("activate",function(e){ e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){return k!==CACHE?caches.delete(k):null;}));})); self.clients.claim(); });
